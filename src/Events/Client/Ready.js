89ccc83d778ceaaf2d85cafd54921e2a890695eb
GamerRaven <jwang@ravenaro.com>
2023-04-27 11:29:15 +0800

    Added Activity

--- src/Events/Client/Ready.js
+++ src/Events/Client/Ready.js
@@ -1,4 +1,5 @@
 const logger = require("../../Utils/Logger/Logger")
+const { ActivityType } = require("discord.js");
 
 module.exports = {
     name: "ready",
@@ -7,6 +8,16 @@ module.exports = {
     async execute(client) {
 
         logger.info(`[READY] Successfully logged in as ${client.user.tag}.`)
+
+        client.user.setPresence({
+            activities: [
+                {
+                    name: `${client.config.prefix}help`,
+                    type: ActivityType.Playing,
+                },
+            ],
+            status: "Online",
+        });
 
     },
 };
